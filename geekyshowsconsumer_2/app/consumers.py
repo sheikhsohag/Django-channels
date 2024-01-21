cc8d3b26193d9570ed06077733a096eb840dc1c7
sheikhsohag <sheikhsohag000@gmail.com>
2024-01-21 13:09:44 +0600

    difference between async & sync

---
+++ geekyshowsconsumer_2/app/consumers.py
@@ -0,0 +1,62 @@
+from channels.consumer import SyncConsumer
+from channels.consumer import AsyncConsumer
+from channels.exceptions import StopConsumer
+from time import sleep
+import asyncio
+
+
+
+# all method are called automatically
+class MySyncConsumer(SyncConsumer):
+    
+    def websocket_connect(self, event):
+        # it is first time called when user connected initially
+        print('web socket connected...',  event)
+        
+        self.send({
+            'type': 'websocket.accept'
+        })
+    
+    def websocket_receive(self, event):
+        # when client sent data
+        print('Massaged Received...',  event)
+        print('Massaged is',  event['text'])
+        for i in range(50):
+            self.send({
+            'type': 'websocket.send',
+            'text': str(i)
+             })
+            sleep(1)
+    
+    def websocket_disconnect(self, event):
+        # it occur when closing connect or lost connect from client side or server side
+        print('disconnected...',  event)
+        raise StopConsumer()
+        
+
+
+# all method are called automatically
+class MyAsyncConsumer(AsyncConsumer):
+    
+    async def websocket_connect(self, event):
+        # it is first time called when user connected initially
+        print('Async web socket connected...',  event)
+        await self.send({
+            'type': 'websocket.accept'
+        })
+    
+    async def websocket_receive(self, event):
+        # when client sent data
+        print('Massaged Received...',  event)
+        print('Async Message is ', event['type'])
+        for i in range(50):
+            await self.send({
+            'type': 'websocket.send',
+            'text': str(i)
+             })
+            await asyncio.sleep(1)
+    
+    async def websocket_disconnect(self, event):
+        # it occur when closing connect or lost connect from client side or server side
+        print('Async disconnected...', event)
+        raise StopConsumer()
