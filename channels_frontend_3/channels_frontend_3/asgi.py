0c735a4f59414953c3f854aa931d737ba9dcf6ab
sheikhsohag <sheikhsohag000@gmail.com>
2024-01-22 12:50:17 +0600

    real data sent server to client

---
+++ channels_frontend_3/channels_frontend_3/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for geekyshowsconsumer_2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekyshowsconsumer_2.settings')
+from channels.routing import ProtocolTypeRouter, URLRouter
+import app.routing
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': URLRouter(
+        app.routing.websocket_urlpatterns
+    )
+})
