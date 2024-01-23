fbda087058b4ac10661163f57b2d4ca1beb384ff
sheikhsohag <sheikhsohag000@gmail.com>
2024-01-23 08:23:32 +0600

    duplex mode completed with static channels

--- channels_redis_layer/app/consumers.py
+++ channels_redis_layer/app/consumers.py
@@ -69,17 +69,50 @@ class MyAsyncConsumer(AsyncConsumer):
     
     async def websocket_connect(self, event):
         print('Async web socket connected...',  event)
+        print("channels Layer...", self.channel_layer)
+        
+        print("channels name...", self.channel_name)
+        
+        # self.channels_layer.group_add()   it deafult async ..
+        # async_to_sync use convert 
+        
+        await self.channel_layer.group_add(
+            'programmers',    #group name
+             self.channel_name  #channels name
+            )
         await self.send({
             'type': 'websocket.accept'
-        })
+    })
 
     
     async def websocket_receive(self, event):
-        print('Massaged Received...',  event)
+        print('Massaged Received...',  event['text'])
+        await self.channel_layer.group_send('programmers', {
+            'type': 'chat.message',
+            'message': event['text']
+        })
+    
+    
+    async def chat_message(self, event):
+        print('Event...---===', event)
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
        
             
     
     async def websocket_disconnect(self, event):
         # it occur when closing connect or lost connect from client side or server side
         print('Async disconnected...', event)
+        
+        print("channels Layer...", self.channel_layer)
+        
+        print("channels name...", self.channel_name)
+        
+        await self.channel_layer.group_discard(
+            'programmers', 
+             self.channel_name
+            )
+        
         raise StopConsumer()
