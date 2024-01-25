5b87c08ddae30fb82cb26941c5613d75560aad4b
sheikhsohag <sheikhsohag000@gmail.com>
2024-01-25 15:27:24 +0600

    genericconsumer and frontend set up

--- channelsConsumer/app/consumers.py
+++ channelsConsumer/app/consumers.py
@@ -1,5 +1,6 @@
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-
+from time import sleep
+import asyncio
 
 class MyWebsocketConsumer(WebsocketConsumer):
     
@@ -10,7 +11,11 @@ class MyWebsocketConsumer(WebsocketConsumer):
         
     def receive(self, text_data=None, bytes_data=None):
         print('Message Received from Client...', text_data)
-        self.send(text_data="Messages from server to Client ")
+        
+        for i in range(10):
+            self.send(text_data=str(i))
+            sleep(1)
+        # self.send(text_data="Messages from server to Client ")
         
         # self.send(bytes_data=data)  #To send binary frame to client
         # self.close(code = 4123)    #errors code
@@ -28,7 +33,9 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
         
     async def receive(self, text_data=None, bytes_data=None):
         print('AsyncWebsocketConsumer Message Received from Client...', text_data)
-        await self.send(text_data="Messages from server to Client ")
+        for i in range(10):
+            await self.send(text_data=str(i))
+            await asyncio.sleep(1)
         
         # self.send(bytes_data=data)  #To send binary frame to client
         # self.close(code = 4123)    #errors code
